fc291afc0ee84653a2b688fd0bf3ba2326973e89
Zykov Artem <skaterpunisher@gmail.com>
2025-07-15 13:42:28 +0300

    Устранена ошибка повторной регистрации background task
    - Убрана дублирующая ручная регистрация BGTaskScheduler.shared.register()
    - Оставлен только SwiftUI .backgroundTask() modifier
    - Упрощена структура кода
    - Исправлена ошибка NSException duplicate registration

--- RoundTimer/RoundTimerApp.swift
+++ RoundTimer/RoundTimerApp.swift
@@ -4,16 +4,6 @@ import BackgroundTasks
 @main
 struct RoundTimerApp: App {
     
-    private static var backgroundTasksRegistered = false
-    
-    init() {
-        // Регистрируем background app refresh только один раз
-        if !Self.backgroundTasksRegistered {
-            registerBackgroundTasks()
-            Self.backgroundTasksRegistered = true
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -22,22 +12,6 @@ struct RoundTimerApp: App {
             // Обработка background app refresh
             await handleBackgroundRefresh()
         }
-    }
-    
-    private func registerBackgroundTasks() {
-        let success = BGTaskScheduler.shared.register(forTaskWithIdentifier: "timer-refresh", using: nil) { task in
-            self.handleAppRefresh(task: task as! BGAppRefreshTask)
-        }
-        if !success {
-            print("Failed to register background task")
-        }
-    }
-    
-    private func handleAppRefresh(task: BGAppRefreshTask) {
-        // Планируем следующий background refresh
-        scheduleAppRefresh()
-        
-        task.setTaskCompleted(success: true)
     }
     
     private func scheduleAppRefresh() {
